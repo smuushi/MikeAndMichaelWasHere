ed78b274c0802198a1c9a39a4af43e84538790c8
smuushi <mishih780@gmail.com>
2023-06-14 14:58:21 -0400

    events refactor

--- index.js
+++ index.js
@@ -30,31 +30,51 @@ for (const file of commandFiles) {
     console.log(client.commands)
 }
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+// client.on(Events.InteractionCreate, async interaction => {
+//     if (!interaction.isChatInputCommand()) return;
 
-    const command = client.commands.get(interaction.commandName)
+//     const command = client.commands.get(interaction.commandName)
 
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.. :(`)
+//     if (!command) {
+//         console.error(`No command matching ${interaction.commandName} was found.. :(`)
+//     }
+
+//     try {
+//         await command.execute(interaction);
+//     } catch (err) {
+//         console.error(err);
+//         if (interaction.replied || interaction.deferred) {
+// 			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+// 		} else {
+// 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 		}
+//     }
+
+//     console.log(interaction)
+// })
+
+// client.once(Events.ClientReady, c => {
+//     console.log(`ayo fam, we did it. logged in as ${c.user.tag}`)
+// })
+// ^^ above has been refactored into ./events
+
+// using that path below now
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for(let i = 0; i < eventFiles.length; i++) {
+    let file = eventFiles[i]
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    if (event.once) {
+        console.log(event.once)
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+
     }
-
-    try {
-        await command.execute(interaction);
-    } catch (err) {
-        console.error(err);
-        if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-    }
-
-    console.log(interaction)
-})
-
-client.once(Events.ClientReady, c => {
-    console.log(`ayo fam, we did it. logged in as ${c.user.tag}`)
-})
+}
 
 client.login(token)
