0a868c9220cf81813d91498595ea8c7720169f60
mcsosa121 <mcsosa121@gmail.com>
2023-06-22 01:54:53 -0400

    can sometimes send images

--- commands/pets.js
+++ commands/pets.js
@@ -1,7 +1,15 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { S3Client, ListObjectsV2Command } = require('@aws-sdk/client-s3');
+/* eslint-disable brace-style */
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const { S3Client, ListObjectsV2Command, GetObjectCommand } = require('@aws-sdk/client-s3');
 
+const { writeFile } = require('fs/promises');
 const client = new S3Client({});
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * max);
+}
 
 // needs to have aws configured on dev env to work
 module.exports = {
@@ -19,23 +27,34 @@ module.exports = {
 		try {
 			let isTruncated = true;
 
-			console.log('Your bucket contains the following objects:\n')
-			let contents = '';
+			const contents = [];
 
 			while (isTruncated) {
 				const { Contents, IsTruncated, NextContinuationToken } = await client.send(command);
-				const contentsList = Contents.map((c) => ` • ${c.Key}`).join('\n');
-				contents += contentsList + '\n';
+				const contentsList = Contents.map((c) => c.Key);
+				contents.push(...contentsList);
 				isTruncated = IsTruncated;
 				command.input.ContinuationToken = NextContinuationToken;
 			}
-			console.log(contents);
-			await interaction.reply(contents);
 
-		}
-		catch (err) {
+			const GetImgCommand = new GetObjectCommand({
+				Bucket: 'michaelspets',
+				Key: contents[getRandomInt(0, contents.length)],
+			});
+
+			const response = await client.send(GetImgCommand);
+            // console.log(response.data);
+			const arr = await response.Body.transformToByteArray();
+			await writeFile('hi.jpg', arr);
+            // const rbody = await response.Body;
+            // const img = await Buffer.from(response.Body.transformToByteArray());
+			//   console.log(response);
+			const attachment = new AttachmentBuilder('hi.jpg', { name: 'profile-image.png' });
+
+			interaction.reply({ files: [attachment] });
+
+		} catch (err) {
 			console.error(err);
 		}
-
 	},
 };
